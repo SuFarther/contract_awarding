4f0421248cb2fe78662683808d4630451f016625
xiaosu <2257346739@qq.com>
2022-08-04 09:10:36 +0800

    vector原理和ArrayList线程安全原理分析，vector的原理是它的add方法加了同步函数synchronized关键字，所以是线程安全的,但是效率很低,因为一个线程进来synchronized每次都会加锁,下个线程要等上个线程解锁了才可以执行。然后ArrayList的add方法没有加synchronized同步函数,也正是这原因,在多线程中会产生数据冲突,线程不安全，但是效率高

---
+++ thread_principle/src/com/onecbuying/vector/ThreadVector.java
@@ -0,0 +1,40 @@
+package com.onecbuying.vector;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName ThreadVector
+ * @company 公司
+ * @Description Vector源码分析
+ * Vector源码部分:
+ * 将指定元素附加到此 Vector 的末尾。 @param e 要附加到此向量的元素
+ * @return {@code true}（由 {@link Collection add} 指定）@since 1.2
+ * public synchronized boolean add(E e) {
+ *         modCount++;
+ *         ensureCapacityHelper(elementCount + 1);
+ *         elementData[elementCount++] = e;
+ *         return true;
+ *     }
+ *  ArrayList源码部分:
+ *   将指定元素附加到此列表的末尾。参数：e - 要附加到此列表的元素返回：true（由 Collection.add 指定）
+ *   public boolean add(E e) {
+ *         ensureCapacityInternal(size + 1);  // Increments modCount!!
+ *         elementData[size++] = e;
+ *         return true;
+ *     }
+ *    没有加synchronized关键字,没有做线程同步,如果加了synchronized每次都要把这个东西锁住，等下个资源竞争
+ *    才做释放锁,所以这个效率就高，在多线程中会产生数据冲突
+ *
+ * @createTime 2022年08月04日 08:41:41
+ */
+public class ThreadVector {
+    public static void main(String[] args) {
+        Vector<?> vector = new Vector<Object>();
+//        vector.add("");
+        ArrayList<?> list = new ArrayList<>();
+//        list.add(1);
+    }
+}

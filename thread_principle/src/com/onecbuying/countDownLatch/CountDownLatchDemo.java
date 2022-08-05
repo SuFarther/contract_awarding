794333b662723f670152bfa7fff6a8f38851d2c9
xiaosu <2257346739@qq.com>
2022-08-05 19:50:07 +0800

    CountDownLatch并发包计数器

--- thread_principle/src/com/onecbuying/countDownLatch/CountDownLatchDemo.java
+++ thread_principle/src/com/onecbuying/countDownLatch/CountDownLatchDemo.java
@@ -31,7 +31,7 @@ public class CountDownLatchDemo {
                 System.out.println("子线程,"+Thread.currentThread().getName()+"结束执行...");
             }
         }).start();
-        countDownLatch.await();
+        countDownLatch.await();// 调用当前方法主线程阻塞  countDown结果为0, 阻塞变为运行状态
         System.out.println("两个子线程执行完毕...");
         System.out.println("继续主线程执行...");
     }
